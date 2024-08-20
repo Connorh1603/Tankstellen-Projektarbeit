04350f265763643c5bec746c57d12504e2495019
Connorh1603 <connorh1603@gmail.com>
2024-08-20 13:44:46 +0200

    App Changes, Consoleview changes

--- src/App.java
+++ src/App.java
@@ -1,28 +1,19 @@
-import java.util.Scanner;
+
 
 import Controller.ChatController;
 import bots.WeatherBot;
+import view.ConsoleView;
 
 public class App {
     public static void main(String[] args) throws Exception {
-            ChatController controller = new ChatController();
+        ChatController controller = new ChatController();
+        
+        // Registrierung der verfügbaren Bots
         controller.registerBot(1, new WeatherBot());
 
-        Scanner scanner = new Scanner(System.in);
-        while (true) {
-            System.out.println("Enter command:");
-            String command = scanner.nextLine();
-            if (command.startsWith("activate bot")) {
-                int botId = Integer.parseInt(command.split(" ")[2]);
-                controller.activateBot(botId);
-            } else if (command.startsWith("deactivate bot")) {
-                int botId = Integer.parseInt(command.split(" ")[2]);
-                controller.deactivateBot(botId);
-            } else if (command.equals("list bots")) {
-                controller.listAvailableBots();
-            } else {
-                System.out.println("Unknown command.");
-            }
-        }
+        
+        // Starten der Konsolenschnittstelle
+        ConsoleView consoleView = new ConsoleView(controller);
+        consoleView.start();
     }
 }
--- src/view/ConsoleView.java
+++ src/view/ConsoleView.java
@@ -1,5 +1,32 @@
 package view;
 
-public class ConsoleView {
+import java.util.Scanner;
 
+import Controller.ChatController;
+
+public class ConsoleView {
+    private ChatController controller;
+
+    public ConsoleView(ChatController controller) {
+        this.controller = controller;
+    }
+
+    public void start() {
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.println("Enter command:");
+            String command = scanner.nextLine();
+            if (command.startsWith("activate bot")) {
+                int botId = Integer.parseInt(command.split(" ")[2]);
+                controller.activateBot(botId);
+            } else if (command.startsWith("deactivate bot")) {
+                int botId = Integer.parseInt(command.split(" ")[2]);
+                controller.deactivateBot(botId);
+            } else if (command.equals("list bots")) {
+                controller.listAvailableBots();
+            } else {
+                System.out.println("Unknown command.");
+            }
+        }
+    }
 }
